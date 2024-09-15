1118874964bf896bb637bf2ed431abbed1086455
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-15 11:10:19 +0700

    Cek API

--- app/Http/Controllers/VisitorsController.php
+++ app/Http/Controllers/VisitorsController.php
@@ -670,6 +670,7 @@ class VisitorsController extends Controller
             CURLOPT_CUSTOMREQUEST => 'GET',
         ));
 
+        dd(env("API_FOOTBALL"));
         $response = curl_exec($curl);
 
         curl_close($curl);
