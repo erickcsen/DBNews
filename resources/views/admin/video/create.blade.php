ce1648e114d7adab51ae8a91418e83e57f6101bf
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-20 22:56:09 +0700

    Perbaikan Error

--- resources/views/admin/video/create.blade.php
+++ resources/views/admin/video/create.blade.php
@@ -70,6 +70,7 @@
                                     function tags_event(e) {
                                         let txt = e.value;
                                         let arry_txt = txt.split("#");
+                                        arry_txt = (arry_txt.length == 0)?txt.split(","):arry_txt;
                                         tags.innerHTML = '';
                                         for (let i = 0; i < arry_txt.length; i++) {
                                             const txt = arry_txt[i];
