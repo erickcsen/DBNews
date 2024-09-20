c0e539b9c341c4a1390c4e5eb2ba4ac03b0a8d68
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-20 10:57:16 +0700

    Update Email Custom

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,9 +5,7 @@
 @if (trim($slot) === 'Laravel')
     <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-    <div style="width:100%;background-color:#4a25a9;padding:10px;border-radius:10px;">
-        <img src="{{asset('/images/logo_dbnews.png')}}" class="logo" alt="{{$slot}} Logo">
-    </div>
+    <img src="{{asset('/images/logo_dbnews.png')}}" class="logo" alt="{{$slot}} Logo">
 @endif
 </a>
 </td>
