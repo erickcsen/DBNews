315763055fa0224001fe2fdfe00734f05e2f27a1
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-20 10:49:30 +0700

    Update Reset Password Email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,9 @@
 @if (trim($slot) === 'Laravel')
     <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-    <img src="{{asset('/images/logo_dbnews.png')}}" class="logo" alt="{{$slot}} Logo">
+    <div style="width:100%;background-color:#4a25a9">
+        <img src="{{asset('/images/logo_dbnews.png')}}" class="logo" alt="{{$slot}} Logo">
+    </div>
 @endif
 </a>
 </td>
