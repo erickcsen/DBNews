d7c6128f925f0e79bf670930c25c9cbdd0b27d1e
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-28 15:11:10 +0700

    Google Ads 1

--- resources/views/visitors/home.blade.php
+++ resources/views/visitors/home.blade.php
@@ -26,31 +26,17 @@
                 <!-- Iklan 1 -->
                 <div class="row">
                     <div class="col-12 my-4" style="height: 200px">
-                        <div class="col-12">
-                            @foreach ($ads_bottom as $item)
-                                <a href="{{ $item->link }}" target="_blank">
-                                    <img class="iklan_bottom_1 rounded" src="{{ Storage::url($item->ad_img) }}"
-                                        style="width:100%">
-                                </a>
-                            @endforeach
-                        </div>
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1326912091817850"
+                            crossorigin="anonymous"></script>
+                            
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-1326912091817850"
+                            data-ad-slot="1265174380"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
                         <script>
-                            var myIndex = 0;
-                            carousel1();
-
-                            function carousel1() {
-                                var i;
-                                var x = document.getElementsByClassName("iklan_bottom_1");
-                                for (i = 0; i < x.length; i++) {
-                                    x[i].style.display = "none";
-                                }
-                                myIndex++;
-                                if (myIndex > x.length) {
-                                    myIndex = 1
-                                }
-                                x[myIndex - 1].style.display = "block";
-                                setTimeout(carousel1, 2000); // Change image every 2 seconds
-                            }
+                            (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
                     </div>
                 </div>
