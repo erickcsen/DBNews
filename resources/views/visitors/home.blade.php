883deac259318115511c4aa17088e3f25314847f
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-28 15:18:40 +0700

    Google AdsSense

--- resources/views/visitors/home.blade.php
+++ resources/views/visitors/home.blade.php
@@ -26,6 +26,8 @@
                 <!-- Iklan 1 -->
                 <div class="row">
                     <div class="col-12 my-4" style="height: 200px">
+                    </div>
+                    <div class="col-12">
                         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1326912091817850"
                             crossorigin="anonymous"></script>
                             
