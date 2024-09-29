bc3350d1ee8b926d5c911317bb75dad190dbd935
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-29 16:55:40 +0700

    Google Adsense

--- resources/views/visitors/home.blade.php
+++ resources/views/visitors/home.blade.php
@@ -20,19 +20,23 @@
 
 
                 @include('visitors.partials.home.today_headline_news')
-                <!-- Bottom -->
-                <ins class="adsbygoogle"
-                    style="display:block"
-                    data-ad-client="ca-pub-1283232183386430"
-                    data-ad-slot="2253693122"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true">
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1283232183386430"
-                        crossorigin="anonymous"></script>
-                </ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                <!-- Adsense -->
+                <div class="adsense-ad">
+                    <!-- Bottom -->
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-1283232183386430"
+                        data-ad-slot="2253693122"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true">
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1283232183386430"
+                            crossorigin="anonymous"></script>
+                    </ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>
+                <!-- Adsense -->
                 @include('visitors.partials.home.berita_terpopuler')
                 <div>&nbsp;</div>
                 @include('visitors.partials.home.youtube')
