4c220bff7b815f26a344aa6a21fc2785d86a8728
erick_c <erick.pecinta.negeri@gmail.com>
2024-09-29 16:41:28 +0700

    Iklan

--- resources/views/visitors/home.blade.php
+++ resources/views/visitors/home.blade.php
@@ -20,6 +20,18 @@
 
 
                 @include('visitors.partials.home.today_headline_news')
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1283232183386430"
+                    crossorigin="anonymous"></script>
+                <!-- Bottom -->
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-1283232183386430"
+                    data-ad-slot="2253693122"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
                 @include('visitors.partials.home.berita_terpopuler')
                 <div>&nbsp;</div>
                 @include('visitors.partials.home.youtube')
