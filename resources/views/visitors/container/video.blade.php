9d37d1aae0e9d5511f06e6d5b9c5f7e0533fc367
erick_c <erick.pecinta.negeri@gmail.com>
2024-10-10 19:01:21 +0700

    Icon DBNews

--- resources/views/visitors/container/video.blade.php
+++ resources/views/visitors/container/video.blade.php
@@ -124,6 +124,12 @@
         <!-- Ad Sense -->
         <title>{{$title_page}}</title>
         <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
